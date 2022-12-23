4445793a648150ed6ed24e7cb3b7fff62231a3be
Jso9946 <119714258+Jso9946@users.noreply.github.com>
2022-12-23 11:15:35 +0800

    Create smallest multiple.cpp

---
+++ smallest multiple.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+
+using namespace std;
+
+int main(){
+  int smallest_multiple = x;
+  bool numissmallestmultiple;
+  for (int i = 1; i < 21; i++){
+    if (x % i = 0) {
+    return true;
+    
+    }else {
+    x *= i}
+    
+  }
+  cout << x;
+  return 0;
+}
